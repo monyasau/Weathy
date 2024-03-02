8aeeb4de21190db413cfd42723de5170e8c534a1
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-03-02 08:02:42 +0100

    added  mock up for weather info container

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,8 +3,13 @@ import { useState } from 'react'
 
 let Main=()=> {
     return (
-        <div className='flex justify-center '>
+        <div className='w-full mt-8 h-[80%]'>
 
+        <div className="max-w-screen-xl mx-auto  items-center  h-full flex">
+<div className=' p-4 mx-auto w-[500px] shadow-lg rounded-3xl bg-[#000000d0] h-[450px]'>
+
+</div>
+        </div>
         </div>
     );
 }

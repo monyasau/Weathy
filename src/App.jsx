79020a91ef5de9072bef72165fd133c9df44d7fb
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-03-02 06:43:34 +0100

    update: moved background image code to app.jsx for dynamic editing

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import TopNav from './components/TopNav'
 function App() {
 
   return (
-    <>
+    <div className="h-[100vh] bg-cover bg-no-repeat bg-[url('https://source.unsplash.com/1600x900/?Lagos')]">
     <TopNav/>
     <Main/>
-    </>
+    </div>
   )
 }
 

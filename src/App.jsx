7d192578c12400e1a0612d8e8944fe662710e740
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-03-03 20:13:36 +0100

    fixed import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Main from './components/Main'
+import Weathy from './components/Weathy'
 import TopNav from './components/TopNav'
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className=" h-[100vh] bg-cover bg-no-repeat bg-[url('https://source.unsplash.com/1600x900/?Lagos')]">
     <TopNav/>
-    <Main/>
+    <Weathy/>
     </div>
   )
 }

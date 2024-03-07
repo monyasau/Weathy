9aa76bd866e93a9e72f14e6d8e32c751a29c7eef
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-03-07 20:23:20 +0100

    Update: Replaced dynamic bg image with static one for offline functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import TopNav from './components/TopNav'
 function App() {
 
   return (
-    <div className=" h-[100vh] bg-cover bg-no-repeat bg-[url('https://source.unsplash.com/1600x900/?Lagos')]">
+    // <div className=" h-[100vh] bg-cover bg-no-repeat bg-[url('https://source.unsplash.com/1600x900/?Lagos')]">
+    <div className=" h-[100vh] bg-cover bg-no-repeat bg-[url('./assets/067.jpg')]">
     <TopNav/>
     <Weathy/>
     </div>

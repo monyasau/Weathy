2d8b9589247c5b405cb3fbc6b120f27d020f4e4e
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-03-13 20:17:03 +0100

    fixed some import issues

--- src/components/Weathy.jsx
+++ src/components/Weathy.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import Backend from './Backend';
-import weatherConditionImage from "../assets/overcastClouds.svg";
+import WeatherInfo from './WeatherInfo';
 
 let Weathy=()=> {
   const responseTemplate={
@@ -54,6 +54,7 @@ let Weathy=()=> {
   const checkInput = (event) => {
     if (event.keyCode===13) {
       // fetchWeather(userInput)
+      setLoading(true)
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${apiKey}`).then(response=>response.json()).then(data=>console.log(data)).catch(error=>console.error('Error:', error))
     } else {
       setUserInput(event.target.value)
@@ -63,7 +64,7 @@ let Weathy=()=> {
     return (
         <div className='w-full mt-8 h-[80%]'>
         <div className="max-w-screen-xl text-[whitesmoke] mx-auto  items-center md:px-0 px-[2%] h-full flex">
-<div className=' px-10 p-14 mx-auto max-w-[500px] shadow-lg rounded-[30px] bg-[#000000d0] h-[470px] w-[100%] '>
+<div className=' px-10 p-14 mx-auto max-w-[500px] shadow-lg rounded-[30px] bg-[#000000d0] max-h-[470px] w-[100%] '>
 <div aria-label='search container' className='flex justify-between bg-[#96969633] p-4 rounded-2xl'>
 <input type="text" placeholder='Enter a city name' autoFocus onKeyUp={()=>checkInput(event)} className='focus:outline-none text-[#eee] text-2xl py-4 w-[90%] bg-transparent rounded-xl'/>
 <button onClick={()=>searchClick?setSearchClick(false):setSearchClick(true)}>
@@ -84,70 +85,7 @@ let Weathy=()=> {
 
 </button>
 </div>
-{loading?
-(
-<div aria-label='weather info'>
-<div aria-label='Weather info main' className='my-4 '>
-    <h1 className='text-3xl font-semibold flex items-center'>
-      <div className=' h-10 w-[70%] mr-4'>Scattered Clouds</div>
-      <div className='h-[90px] w-[90px] rounded-full' aria-label='Weather info'><img src={weatherConditionImage} className='w-full h-full' alt="" /></div>
-    </h1>
-      <div className=' rounded-xl w-2/5 text-3xl' aria-label='temp'>37.89°C </div>
-      <div className=' h-5 rounded-md my-3 w-3/5 inline-flex ' aria-label='location'>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-4">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-</svg> Katsina, NG
-</div>
-{/* {useEffect(() => {
-
-  
-
-  return () => {
-    
-<Backend location={userInput}/>
-  }
-}, [userInput])} */}
-
-</div>
-<div aria-label='weather info footer' className=' flex text-2xl items-cent justify-ce font-semibold w-full '>
-    <div className='w-[50%]'>Feels like 36°C</div>
-  <div className='w-[50%] text-right'>Feels like 36°C</div>
-  </div>
-</div>
-)
-:(
- <div aria-label='skeleton weather info'>
-
-<div aria-label='Weather info main' className='my-4'>
-    <h1 className='text-3xl font-semibold flex items-center'>
-      <div className='bg-slate-200 h-12 rounded-xl w-[70%] mr-4 animate-pulse'></div>
-      <div className='bg-slate-200 h-[90px] w-[90px] rounded-full animate-pulse' aria-label='Weather info'></div>
-    </h1>
-      <div className='bg-slate-200 h-10 rounded-xl w-2/5 animate-pulse' aria-label='temp'></div>
-      <div className='bg-slate-200 h-6 rounded-md my-3 w-3/5 animate-pulse' aria-label='location'></div>
-{useEffect(() => {
-
-
-  return () => {
-    
-<Backend location={userInput}/>
-  }
-}, [userInput])}
-
-
-</div>
-<div aria-label='weather info footer' className=' grid grid-cols-2 gap-3 w-full animate-pulse'>
-  <div className='h-10 text-center p-12 border-r bg-slate-200  rounded-md'>
-    <div className='h-8 animate-pulse rounded-lg'></div>
-  </div>
-  <div className='h-10 text-center p-12 border-l bg-slate-200 rounded-md'>
-    <div className='h-8 animate-pulse rounded-lg'></div>
-  </div>
-</div>
-</div> 
-)
-}
+<WeatherInfo loading={loading} />
 </div>
         </div>
         </div>
